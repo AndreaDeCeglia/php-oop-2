3d06b1cc72f19ff8d15a0419f2adcb5afdae7063
AndreaDeCeglia <deceglia89@gmail.com>
2022-12-19 14:54:18 +0100

    toys.php done

--- classes/toys.php
+++ classes/toys.php
@@ -1,5 +1,24 @@
 <?php 
 
+include_once __DIR__ . '/classes/product.php';
 
+class toys extends product{    
+    public $features;
+    public $dimentions;
+
+    public function __construct(
+        String $image,
+        String $productName,
+        Float $price,
+        categories $category,
+
+        String $features,
+        String $dimentions
+    ) {
+        $this->features = $features;
+        $this->dimentions = $dimentions;
+        parent::__construct($image,$productName,$price,$category)
+    }
+}
 
 ?>
