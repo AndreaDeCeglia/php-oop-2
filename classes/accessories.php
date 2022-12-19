6034bcc33537d192f3ec0803d14543a1fe390aae
AndreaDeCeglia <deceglia89@gmail.com>
2022-12-19 14:50:22 +0100

    accessories.php done

--- classes/accessories.php
+++ classes/accessories.php
@@ -1,5 +1,24 @@
 <?php 
 
+include_once __DIR__ . '/classes/product.php';
 
+class accessories extends product{    
+    public $materials;
+    public $dimentions;
+
+    public function __construct(
+        String $image,
+        String $productName,
+        Float $price,
+        categories $category,
+
+        String $materials,
+        String $dimentions
+    ) {
+        $this->materials = $materials;
+        $this->dimentions = $dimentions;
+        parent::__construct($image,$productName,$price,$category)
+    }
+}
 
 ?>
